6e499ed1a561f8c600035947511420ca90426418
Mansi More <92293378+MansiMore99@users.noreply.github.com>
2022-07-24 18:42:08 +0530

    Add files via upload

---
+++ topics/src/main/java/com/example/topics/Topic.java
@@ -0,0 +1,44 @@
+package com.example.topics;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Topic {
+
+    @Id
+
+    String id;
+    String name;
+    String desc;
+
+
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String desc) {
+        super();
+    this.id=id;
+    this.name=name;
+    this.desc=desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    }
